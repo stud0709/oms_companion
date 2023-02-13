e34b4367c6970cabd0a71206f26c430d00104592
stud0709 <31960113+stud0709@users.noreply.github.com>
2023-02-13 11:45:18 +0100

    context save actions from QRFrame

--- omscompanion/src/main/java/omscompanion/QRFrame.java
+++ omscompanion/src/main/java/omscompanion/QRFrame.java
@@ -5,19 +5,30 @@ import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import java.util.List;
 import java.util.TimerTask;
+import java.util.concurrent.atomic.AtomicBoolean;
 
+import javax.imageio.stream.FileImageOutputStream;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPopupMenu;
 import javax.swing.SwingUtilities;
 
 import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
 
 public class QRFrame extends JFrame {
 	private static final long serialVersionUID = -6749732054569148998L;
@@ -25,9 +36,12 @@ public class QRFrame extends JFrame {
 	private final JLabel lbl;
 	private final Runnable andThen;
 	public static final int AUTOCLOSE = 60_000;
+	private final String message;
+	private final AtomicBoolean cleanupOnDeactivate = new AtomicBoolean(true);
 
 	public QRFrame(String message, long delay, Runnable andThen)
 			throws NoSuchAlgorithmException, IOException, WriterException {
+		this.message = message;
 
 		AnimatedQrHelper qrHelper = new AnimatedQrHelper(message, delay, () -> instance == QRFrame.this,
 				bi -> setImage(bi));
@@ -71,7 +85,8 @@ public class QRFrame extends JFrame {
 
 				@Override
 				public void windowDeactivated(WindowEvent e) {
-					cleanup();
+					if (cleanupOnDeactivate.get())
+						cleanup();
 				}
 			});
 			addComponentListener(new ComponentAdapter() {
@@ -86,6 +101,30 @@ public class QRFrame extends JFrame {
 
 			lbl = new JLabel(new ImageIcon(qrHelper.getImages().get(0)));
 			add(lbl, BorderLayout.CENTER);
+
+			JPopupMenu menu = new JPopupMenu();
+
+			JMenuItem saveGif = new JMenuItem("\u21e9 GIF");
+			saveGif.addActionListener(e -> onBtnGif());
+			menu.add(saveGif);
+
+			JMenuItem saveGifBase64 = new JMenuItem("\u21e9 BASE64 GIF");
+			saveGifBase64.addActionListener(e -> onBtnImg());
+			menu.add(saveGifBase64);
+
+			JMenuItem saveMessage = new JMenuItem("\u21e9 " + MessageComposer.OMS_PREFIX);
+			saveMessage.addActionListener(e -> onBtnText());
+			menu.add(saveMessage);
+
+			lbl.addMouseListener(new MouseAdapter() {
+				@Override
+				public void mouseClicked(java.awt.event.MouseEvent e) {
+					if (SwingUtilities.isRightMouseButton(e)) {
+						menu.show(QRFrame.this, e.getX(), e.getY());
+					}
+				};
+			});
+
 			pack();
 
 			instance = this;
@@ -120,5 +159,80 @@ public class QRFrame extends JFrame {
 			}
 			dispose();
 		}
+	}
+
+	private File generateGif() {
+		File f = new File("qr.gif");
+		f.deleteOnExit();
+
+		try (FileImageOutputStream fios = new FileImageOutputStream(f)) {
+			List<BitMatrix> list = QRUtil.getQrSequence(message, QRUtil.CHUNK_SIZE, QRUtil.BARCODE_SIZE);
+			AnimatedGifWriter.createGif(list, fios, AnimatedQrHelper.DELAY);
+			fios.flush();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+
+			cleanupOnDeactivate.set(false);
+			JOptionPane.showMessageDialog(this, ex.getMessage());
+			cleanupOnDeactivate.set(true);
+
+			return null;
+		}
+
+		return f;
+	}
+
+	private void onBtnGif() {
+		File f = generateGif();
+
+		if (f == null)
+			return;
+
+		ClipboardUtil.set(f);
+
+		cleanupOnDeactivate.set(false);
+		JOptionPane.showMessageDialog(this,
+				"QR code sequence has been generated and copied to the clipboard as GIF image");
+		cleanupOnDeactivate.set(true);
+	}
+
+	private void onBtnImg() {
+		File f = generateGif();
+
+		if (f == null)
+			return;
+
+		try {
+			byte[] bArr = Files.readAllBytes(f.toPath());
+			ClipboardUtil.set(Base64.getEncoder().encodeToString(bArr));
+		} catch (IOException ex) {
+			ex.printStackTrace();
+
+			cleanupOnDeactivate.set(false);
+			JOptionPane.showMessageDialog(this, ex.getMessage());
+			cleanupOnDeactivate.set(true);
+
+			return;
+		}
+
+		cleanupOnDeactivate.set(false);
+		JOptionPane.showMessageDialog(this,
+				"QR code sequence has been generated and copied to the clipboard as BASE64");
+		cleanupOnDeactivate.set(true);
+
+	}
+
+	private void onBtnText() {
+		createTextLink();
+
+		cleanupOnDeactivate.set(false);
+		JOptionPane.showMessageDialog(this,
+				"Data (" + MessageComposer.OMS_PREFIX + "...) has been copied to the clipboard");
+		cleanupOnDeactivate.set(true);
+	}
+
+	private void createTextLink() {
+		String omsURL = MessageComposer.encodeAsOmsText(message);
+		ClipboardUtil.set(omsURL);
 	}
 }
